763ec4ea41bedc93d09a6752ef816cd6a8603b02
ranzhejiang <1689711298@qq.com>
2020-05-29 18:53:22 +0800

    add print

--- xmlrpc/asyn_multiprocess_rpc/client.py
+++ xmlrpc/asyn_multiprocess_rpc/client.py
@@ -4,6 +4,7 @@ import time
 if __name__ == '__main__':
     c = rpclient.RPCClient()
     c.connect('127.0.0.1', 7042)
+    print("Do you feel well")
     # res2 = c.add(2,100,c=5)
     # print(f'res2: [{res2}]')
     for i in range(10):
